c7687495f837949bca0fbbfddc3d88a2620fffc6
zhengxin <43071004+guanjianhe@users.noreply.github.com>
2019-05-28 00:00:01 +0800

    Update led.h

--- led.h
+++ led.h
@@ -3,8 +3,8 @@
 
 #include "stm32f10x.h"
 
-extern void led_init (void);
-extern int led_set(uint8_t led_id,uint8_t state);
+extern void led_init(void);
+extern int led_set(uint8_t led_id, uint8_t state);
 extern int led_on(uint8_t led_id);
 extern int led_off(uint8_t led_id);
 extern int led_toggle(uint8_t led_id);
