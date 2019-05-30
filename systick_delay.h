66d66ee61dd082af1ba538934d279fdc3310cf98
zhengxin <43071004+guanjianhe@users.noreply.github.com>
2019-05-30 18:05:43 +0800

    Create systick_delay.h

---
+++ systick_delay.h
@@ -0,0 +1,13 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+#include "stm32f10x.h"
+
+extern void systick_init(void);
+
+extern void systick_delay_us(uint32_t nms);
+
+extern void systick_delay_ms(uint16_t nms);
+
+
+#endif
